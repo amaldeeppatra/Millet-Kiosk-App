a4cc3ab6623c089474edcb02e1a6f4857344617e
Amaldeep <amaldeeppatra21@gmail.com>
2025-01-18 20:14:34 +0530

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 import LandingPage from './pages/LandingPage'
 import LogInWithPhone from './pages/LogInWithPhone'
+import { createBrowserRouter,Outlet,RouterProvider } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import LoginGoogle from './pages/LoginGoogle'
 
-function App() {
-  return (
-    <>
-      {/* <LandingPage/> */}
-      <LogInWithPhone/>
-    </>
+
+const App=()=>{
+  return(
+      <>
+      <Outlet />
+      </>
+     
   )
 }
 
-export default App
+const appRouter = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:<App/>,
+      children:[
+        {
+          path:"/",
+          element:[<LandingPage/>]
+        },
+        {
+          path:"/login",
+          element:[<LoginGoogle/>]
+        },
+        {
+          path:"/login-phone",
+          element:[<LogInWithPhone/>]
+        }
+      ]
+    }
+  ]
+)
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
